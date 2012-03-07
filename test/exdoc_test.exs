c5c08d4106419a61767fc6a5f2b6836e53823923
Rafael França + Carlos Galdino <rafael.franca+carlos.galdino@plataformatec.com.br>
2012-03-07 17:48:51 -0300

    ExDoc.get_docs tests for moduledoc info

--- test/exdoc_test.exs
+++ test/exdoc_test.exs
@@ -7,4 +7,18 @@ defmodule ExDocTest do
     file = File.expand_path("../fixtures/::Enum.beam", __FILE__)
     assert_match [{ "::Enum", _ }], ExDoc.get_docs([file])
   end
+
+  test "get_docs returns the moduledoc info" do
+    file = File.expand_path("../fixtures/::Enum.beam", __FILE__)
+    [{ _, {moduledoc, _} }] = ExDoc.get_docs([file])
+    { _, doc_text } = moduledoc
+    assert is_binary(doc_text)
+  end
+
+  test "get_docs returns nil if there's no moduledoc info" do
+    file = File.expand_path("../fixtures/::ArgumentError.beam", __FILE__)
+    [{ _, {moduledoc, _} }] = ExDoc.get_docs([file])
+    { _, doc_text } = moduledoc
+    assert_nil doc_text
+  end
 end
