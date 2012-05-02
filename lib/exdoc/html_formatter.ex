7be7875e13b3affc244fb31d7023b0247077900a
Sergey Nartimov <just.lest@gmail.com>
2012-05-02 23:27:27 +0300

    add link to function source
    hardcoded to elixir github repo for now

--- lib/exdoc/html_formatter.ex
+++ lib/exdoc/html_formatter.ex
@@ -1,6 +1,6 @@
 defmodule ExDoc.HTMLFormatter do
-  def format_docs({name,{ moduledoc, docs }}, output_path) do
-    docs = generate_html_for_docs(docs)
+  def format_docs({name, source_path, { moduledoc, docs }}, output_path) do
+    docs = generate_html_for_docs(source_path, docs)
     moduledoc = generate_html_for_moduledoc(moduledoc)
     function_docs = Enum.filter_map docs, fn(x, do: filter_by_type(x, :def)), fn(x, do: get_content(x))
     macro_docs = Enum.filter_map docs, fn(x, do: filter_by_type(x, :defmacro)), fn(x, do: get_content(x))
@@ -33,15 +33,16 @@ defmodule ExDoc.HTMLFormatter do
     Markdown.to_html(doc)
   end
 
-  defp generate_html_for_docs(docs) do
-    Enum.map docs, extract_docs(&1)
+  defp generate_html_for_docs(source_path, docs) do
+    Enum.map docs, extract_docs(source_path, &1)
   end
 
-  defp extract_docs({ { name, arity }, _line, type, doc }) do
+  defp extract_docs(source_path, { { name, arity }, line, type, doc }) do
     html = if doc, do: Markdown.to_html(doc), else: ""
 
     function_name = "#{name}/#{arity}"
-    content = %b{<div class="function"><div class="function-title" id="#{function_name}">\n<b>#{function_name}</b>\n</div>\n<div class="description">\n#{html}</div>\n</div>\n}
+    source_link = %b{<a href="https://github.com/elixir-lang/elixir/blob/master/#{source_path}#L#{line}" target="_blank">Source</a>}
+    content = %b{<div class="function"><div class="function-title" id="#{function_name}">\n<b>#{function_name}</b>\n</div>\n<div class="description">\n#{html}</div>\n#{source_link}</div>\n}
     { type, content }
   end
 
