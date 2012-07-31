249249db00f32075d3e1f2a2711768df015eee57
José Valim <jose.valim@plataformatec.com.br>
2012-07-31 15:16:20 +0200

    Update to elixir master

--- lib/ex_doc/cli.ex
+++ lib/ex_doc/cli.ex
@@ -1,6 +1,6 @@
 defmodule ExDoc.CLI do
   def run(args) do
-    { options, argv } = OptionParser.Simple.parse(args, [o: :output, f: :formatter, p: :project_url])
+    { options, argv } = OptionParser.parse(args, aliases: [o: :output, f: :formatter, p: :project_url])
     path = Enum.first(argv) || "ebin"
     ExDoc.generate_docs(path, options)
   end
