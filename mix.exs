95bf40ee1377c26beadb593a79a409478939bdc6
José Valim <jose.valim@plataformatec.com.br>
2013-08-18 21:45:59 +0200

    Specify proper Elixir version

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule ExDoc.Mixfile do
   def project do
     [ app: :ex_doc,
       version: "0.1.0",
-      elixir: "~> 0.10.2",
+      elixir: "~> 0.10.2-dev",
       compilers: [:sundown, :elixir, :app],
       source_url: "https://github.com/elixir-lang/ex_doc/"
     ]
