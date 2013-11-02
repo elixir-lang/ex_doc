3be009f28fa6012688138abe34b74d3fb394e4a6
José Valim <jose.valim@plataformatec.com.br>
2013-11-02 10:26:14 +0100

    Update to latest Elixir

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule ExDoc.Mixfile do
   def project do
     [ app: :ex_doc,
       version: "0.1.0",
-      elixir: "~> 0.10.3-dev",
+      elixir: "~> 0.11.0",
       compilers: [:sundown, :elixir, :app],
       source_url: "https://github.com/elixir-lang/ex_doc/"
     ]
