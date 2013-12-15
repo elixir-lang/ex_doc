730637575d974485f7a377b61ce89f50ef71a170
José Valim <jose.valim@plataformatec.com.br>
2013-12-15 12:12:14 +0100

    Improve error message

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mix.Tasks.Compile.Sundown do
 
   def run(_) do
     if Mix.shell.cmd("make priv/markdown.so") != 0 do
-      raise Mix.Error, message: "could not compile markdown.so"
+      raise Mix.Error, message: "could not run `make priv/markdown.so`. Do you have make and gcc installed?"
     end
   end
 end
