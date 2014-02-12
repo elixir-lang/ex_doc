b5285aed8385db1212ca366c6efc94389d7f8441
José Valim <jose.valim@plataformatec.com.br>
2014-02-12 10:48:19 +0100

    Update Elixir dependency

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ExDoc.Mixfile do
   def project do
     [ app: :ex_doc,
       version: "0.1.0",
-      elixir: "~> 0.12.3",
+      elixir: "~> 0.12.4",
       compilers: [:sundown, :elixir, :app],
       source_url: "https://github.com/elixir-lang/ex_doc/"
     ]
