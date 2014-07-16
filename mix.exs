54b0a5afa22471537d98fde9bba1cd4f70066c39
pragdave <dave@pragprog.com>
2014-07-16 13:35:45 -0500

    Add support for earmark, so we no longer rely on external markdown processors

--- mix.exs
+++ mix.exs
@@ -2,14 +2,19 @@ defmodule ExDoc.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex_doc,
-     version: "0.5.0-dev",
-     elixir: "~> 0.14.3",
-     deps: deps,
-     source_url: "https://github.com/elixir-lang/ex_doc/"]
+    [
+     app:        :ex_doc,
+     version:    "0.5.1-dev",
+     elixir:     "~> 0.14.3",
+     deps:       deps,
+     source_url: "https://github.com/elixir-lang/ex_doc/"
+    ]
   end
 
   defp deps do
-    [{:markdown, github: "devinus/markdown", only: [:dev, :test]}]
+    [
+     {:earmark, ">= 0.1.4", git: "git://github.com/pragdave/earmark", only: [:dev, :test]}
+#     {:earmark, ">= 0.1.4", only: [:dev, :test]}
+    ]
   end
 end
