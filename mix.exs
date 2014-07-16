50949f59d72bf2f862546e44eb8124b60d336d8f
José Valim <jose.valim@plataformatec.com.br>
2014-07-16 22:11:55 +0200

    Add devinus/markdown for testing, include hex on builds

--- mix.exs
+++ mix.exs
@@ -2,18 +2,15 @@ defmodule ExDoc.Mixfile do
   use Mix.Project
 
   def project do
-    [
-     app:        :ex_doc,
+    [app:        :ex_doc,
      version:    "0.5.1-dev",
      elixir:     "~> 0.14.3",
      deps:       deps,
-     source_url: "https://github.com/elixir-lang/ex_doc/"
-    ]
+     source_url: "https://github.com/elixir-lang/ex_doc/"]
   end
 
   defp deps do
-    [
-     {:earmark, ">= 0.1.4", only: [:dev, :test]}
-    ]
+    [{:earmark, ">= 0.1.4", only: [:dev, :test]},
+     {:markdown, github: "devinus/markdown", only: [:test]}]
   end
 end
