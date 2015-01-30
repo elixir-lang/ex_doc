elixir-lang/ex_doc

commit 5b520c4cad67c3a4b55042ca588ecfb5bea0b41f

Do not use regexes to escape html

lib/ex_doc/formatter/html/templates.ex
@@ -69,8 +69,10 @@ defmodule ExDoc.Formatter.HTML.Templates do
   defp presence(other), do: other
 
   defp h(binary) do
-    escape_map = [{ ~r(&), "\\&amp;" }, { ~r(<), "\\&lt;" }, { ~r(>), "\\&gt;" }, { ~r("), "\\&quot;" }]
-    Enum.reduce escape_map, binary, fn({ re, escape }, acc) -> Regex.replace(re, acc, escape) end
+    escape_map = [{"&", "&amp;"}, {"<", "&lt;"}, {">", "&gt;"}, {"\"", "&quot;"}]
+    Enum.reduce escape_map, binary, fn({pattern, escape}, acc) ->
+      String.replace(acc, pattern, escape)
+    end
   end
 
   # Get the breadcrumbs HTML.

lib/ex_doc/formatter/html/autolink.ex
@@ -7,15 +7,6 @@ defmodule ExDoc.Formatter.HTML.Autolink do
   @erlang_docs "http://www.erlang.org/doc/man/"
 
   @doc """
-  Escape `'`, `"`, `&`, `<` and `>` in the string using HTML entities.
-  This is only intended for use by the HTML formatter.
-  """
-  def escape_html(binary) do
-    escape_map = [{ ~r(&), "\\&amp;" }, { ~r(<), "\\&lt;" }, { ~r(>), "\\&gt;" }, { ~r("), "\\&quot;" }]
-    Enum.reduce escape_map, binary, fn({ re, escape }, acc) -> Regex.replace(re, acc, escape) end
-  end
-
-  @doc """
   Receives a list of module nodes and autolink all docs and typespecs.
   """
   def all(modules) do

commit be726914dd2e12fb40217b42d4948b363dc3f8b1

Fix the HTML escaping broken by 5b520c4

lib/ex_doc/formatter/html/templates.ex
@@ -69,7 +69,12 @@ defmodule ExDoc.Formatter.HTML.Templates do
   defp presence(other), do: other
 
   defp h(binary) do
-    escape_map = [{"&", "&amp;"}, {"<", "&lt;"}, {">", "&gt;"}, {"\"", "&quot;"}]
+    escape_map = [
+      {"&", "\\&amp;"},
+      {"<", "\\&lt;"},
+      {">", "\\&gt;"},
+      {"\"", "\\&quot;"}
+    ]
     Enum.reduce escape_map, binary, fn({pattern, escape}, acc) ->
       String.replace(acc, pattern, escape)
     end