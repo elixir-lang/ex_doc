aa1d0b2020ebf08a7661f08b657f6551ace25389
José Valim <jose.valim@plataformatec.com.br>
2015-08-08 09:53:03 +0200

    Release v0.8.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v0.8.0
+
+* Enhancements
+  * New and responsive layout without frames
+
 ## v0.7.3
 
 * Bug fixes
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExDoc.Mixfile do
 
   def project do
     [app:        :ex_doc,
-     version:    "0.7.3",
+     version:    "0.8.0",
      elixir:     "~> 1.0",
      deps:       deps,
      aliases:    aliases,
