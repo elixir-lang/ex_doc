5696f0fc341dda200c52ce863133a44b0f68d2f1
dignifiedquire <dignifiedquire@gmail.com>
2015-09-13 01:10:16 +0200

    [sidebar] Add functions back into the sidebar.
    Closes #356

--- assets/test/helpers.spec.js
+++ assets/test/helpers.spec.js
@@ -8,4 +8,19 @@ describe('helpers', () => {
       expect(escapeText('hello-world')).to.be.equal('hello\\-world')
     })
   })
+
+  describe('findSidebarCategory', () => {
+    it('finds the correct category', () => {
+      const nodes = [{
+        callbacks: [{anchor: 'hello'}],
+        functions: [{anchor: 'world'}]
+      }, {
+        callbacks: [{anchor: 'one'}],
+        macros: [{anchor: 'two'}]
+      }]
+
+      expect(helpers.findSidebarCategory(nodes, 'world')).to.be.eql('functions')
+      expect(helpers.findSidebarCategory(nodes, 'something')).to.be.eql(undefined)
+    })
+  })
 })
