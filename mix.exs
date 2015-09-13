cb464df727912dec9b1963e26d7939b49b0ed658
Milton Mazzarri <milmazz@gmail.com>
2015-09-13 14:32:20 -0500

    Add ExCoveralls as a coverage report tool

--- mix.exs
+++ mix.exs
@@ -2,18 +2,21 @@ defmodule ExDoc.Mixfile do
   use Mix.Project
 
   def project do
-    [app:        :ex_doc,
-     version:    "0.9.1-dev",
-     elixir:     "~> 1.0",
-     deps:       deps,
-     aliases:    aliases,
-     package:    package,
-     source_url: "https://github.com/elixir-lang/ex_doc/"]
+    [app:               :ex_doc,
+     version:           "0.9.1-dev",
+     elixir:            "~> 1.0",
+     deps:              deps,
+     aliases:           aliases,
+     package:           package,
+     source_url:        "https://github.com/elixir-lang/ex_doc/",
+     test_coverage:     [tool: ExCoveralls],
+     preferred_cli_env: [coveralls: :test]]
   end
 
   defp deps do
     [{:earmark, "~> 0.1.17 or ~> 0.2", optional: true},
-     {:markdown, github: "devinus/markdown", only: [:test]}]
+     {:markdown, github: "devinus/markdown", only: [:test]},
+     {:excoveralls, "~> 0.3", only: :test}]
   end
 
   defp aliases do
