6c920028d6acd55dcfa24af7eb1eb1e76f902b27
eksperimental <eksperimental@users.noreply.github.com>
2015-09-25 15:35:26 +0700

    version bump to 0.10.1-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExDoc.Mixfile do
 
   def project do
     [app: :ex_doc,
-     version: "0.10.0",
+     version: "0.10.1-dev",
      elixir: "~> 1.0",
      deps: deps,
      aliases: aliases,
