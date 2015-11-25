b439f3c72b27eee770e0f408560b586de4255efc
José Valim <jose.valim@plataformatec.com.br>
2015-11-25 12:50:53 +0100

    Start 0.11.2-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExDoc.Mixfile do
 
   def project do
     [app: :ex_doc,
-     version: "0.11.1",
+     version: "0.11.2-dev",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      deps: deps,
