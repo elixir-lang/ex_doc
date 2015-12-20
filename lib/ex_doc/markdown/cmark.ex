de2742db2e01ed37aae725845c16a6b04c214fee
Tom von Schwerdtner <tvon@covermymeds.com>
2015-12-20 13:59:07 -0500

    Add support for cmark.ex markdown processing.

---
+++ lib/ex_doc/markdown/cmark.ex
@@ -0,0 +1,20 @@
+defmodule ExDoc.Markdown.Cmark do
+  @moduledoc """
+  ExDoc extension for the Cmark Markdown parser
+  """
+
+  @doc """
+  Check if the Cmark Markdown parser module is available. Otherwise, try to
+  load the module
+  """
+  def available? do
+    Code.ensure_loaded?(Cmark)
+  end
+
+  @doc """
+  Generate HTML output. Cmark takes no options.
+  """
+  def to_html(text, _opts \\ []) do
+    Cmark.to_html(text)
+  end
+end
