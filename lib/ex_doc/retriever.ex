e1cd84e74a176746d855b084bf1c88851716e8b8
Tomasz Marek Sulima <eiji7@cryptolab.net>
2017-07-18 08:14:54 +0200

    fixes: #755

--- lib/ex_doc/retriever.ex
+++ lib/ex_doc/retriever.ex
@@ -241,6 +241,12 @@ defmodule ExDoc.Retriever do
             |> Enum.map(&Typespec.spec_to_ast(name, &1))
             |> Enum.reverse()
 
+    specs = if type == :defmacro do
+      Enum.map(specs, &remove_first_macro_arg/1)
+    else
+      specs
+    end
+
     annotations =
       case {type, name, arity} do
         {:defmacro, _, _} ->
@@ -274,6 +280,10 @@ defmodule ExDoc.Retriever do
       annotations: annotations
     }
   end
+
+  defp remove_first_macro_arg(
+    {:::, info, [{name, info2, [_term_arg | rest_args]}, return]}
+  ), do: {:::, info, [{name, info2, rest_args}, return]}
 
   defp get_defaults(signature, name, arity) do
     case Enum.count(signature, &match?({:\\, _, [_, _]}, &1)) do
