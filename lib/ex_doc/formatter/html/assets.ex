d709ab86959562651aec8852fbc1c9a9f964caa8
José Valim <jose.valim@plataformatec.com.br>
2017-10-05 20:57:33 +0200

    Remove assets from priv
    Since they are embedded at compile time, they don't
    need to be copied to build.
    
    We also move to yarn to guarantee deterministic assets
    between developers.

--- lib/ex_doc/formatter/html/assets.ex
+++ lib/ex_doc/formatter/html/assets.ex
@@ -1,10 +1,8 @@
 defmodule ExDoc.Formatter.HTML.Assets do
   @moduledoc false
 
-  @assets_dir Application.app_dir(:ex_doc, "priv/ex_doc/formatter/html/assets")
-
   defmacrop embed_pattern(pattern) do
-    [@assets_dir, pattern]
+    ["formatters/html", pattern]
     |> Path.join()
     |> Path.wildcard()
     |> Enum.map(&{Path.basename(&1), File.read!(&1)})
