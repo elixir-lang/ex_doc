d97e842d5472f0382a88df8c6af13b14dbb6853f
tmbb <tmbb@campus.ul.pt>
2017-10-08 03:16:05 +0100

    Fixed extra whitespace

--- lib/ex_doc/markdown_processor.ex
+++ lib/ex_doc/markdown_processor.ex
@@ -7,13 +7,13 @@ defmodule ExDoc.MarkdownProcessor do
   Converts markdown into HTML.
   """
   @callback to_html(String.t, Keyword.t) :: String.t
-  
+
   @doc """
   Assets specific to the markdown implementation.
-  
+
   This callback takes the documentation format (`:html` or `epub`) as an argument
   and must return a list of pairs of the form: `{basename, content}` where:
-  
+
   - `basename` relative path that will be written inside the `doc/` directory.
   - `content` is a binary with the full contents of the file that will be written to `basename`.
 
@@ -21,7 +21,7 @@ defmodule ExDoc.MarkdownProcessor do
       def callback before_closing_head_tag(:html) do
         [{"dist/custom.css", custom_css_content()},
          {"dist/custom.js", custom_js_content()}]
-      end 
+      end
   """
   @callback assets(atom) :: [{String.t, String.t}]
 
@@ -36,10 +36,10 @@ defmodule ExDoc.MarkdownProcessor do
       def callback before_closing_head_tag(:html) do
         # Include the CSS specified in the assets/1 callback
         ~S(<link rel="stylesheet" href="dist/custom.css"/>)
-      end 
+      end
   """
   @callback before_closing_head_tag(atom) :: String.t
-  
+
   @doc """
   Literal content to be written to the file just before the closing body tag.
 
@@ -47,12 +47,12 @@ defmodule ExDoc.MarkdownProcessor do
   and returns a literal string.
   It is useful when the markdown processor needs to a include extra Javascript.
   The result of this function is written directly to the output file.
-  
+
   ## Example
       def callback before_closing_body_tag(:html) do
         # Include the Javascript specified in the assets/1 callback
         ~S(<link rel="stylesheet" href="dist/custom.js"/>)
-      end 
+      end
   """
   @callback before_closing_body_tag(atom) :: String.t
 
