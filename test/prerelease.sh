2464a27d8614694da865cd90452302caa6c67ceb
José Valim <jose.valim@plataformatec.com.br>
2017-10-13 18:02:01 +0200

    Run prerelease script on travis

--- test/prerelease.sh
+++ test/prerelease.sh
@@ -4,8 +4,10 @@ PKG=test/tmp/package
 # build package
 mix hex.build
 
+TAR=$(ls ex_doc-*.tar | head -n 1)
+
 # extract package
-rm -rf $PKG && mkdir -p $PKG/contents && tar xf ex_doc-*.tar -C $PKG && tar xzf $PKG/contents.tar.gz -C $PKG/contents
+rm -rf $PKG && mkdir -p $PKG/contents && tar xf $TAR -C $PKG && tar xzf $PKG/contents.tar.gz -C $PKG/contents
 
 # compile and build docs
 cd $PKG/contents && MIX_ENV=prod mix do deps.get --only prod, compile, docs
