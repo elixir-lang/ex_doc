362edc7d858296695f0aff68b4efd0d9022544da
Nathan Willson <willson.b.nathan@gmail.com>
2018-05-31 03:23:25 -0700

    Link directly to page if sidebar item has no subitems (#855)

---
+++ assets/js/template-helpers/isEmptyNode.js
@@ -0,0 +1,17 @@
+export default function (node, options) {
+  var nodeItems = [
+    node.headers,
+    node.types,
+    node.functions,
+    node.macros,
+    node.callbacks
+  ]
+
+  for (var i = 0; i < nodeItems.length; i++) {
+    if (Array.isArray(nodeItems[i]) && (nodeItems[i].length > 0)) {
+      return options.inverse(this)
+    }
+  }
+
+  return options.fn(this)
+}
