1cfd1317753f1c451764b14d15f732434b487f73
José Valim <jose.valim@plataformatec.com.br>
2018-07-15 18:38:08 +0200

    Relax makeup_elixir dependency

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule ExDoc.Mixfile do
   defp deps do
     [
       {:earmark, "~> 1.1"},
-      {:makeup_elixir, "~> 0.7.0"},
+      {:makeup_elixir, "~> 0.7"},
       {:cmark, "~> 0.5", only: :test},
       {:excoveralls, "~> 0.3", only: :test}
     ]
