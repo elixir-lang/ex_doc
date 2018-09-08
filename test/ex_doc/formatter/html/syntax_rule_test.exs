3baf5f62ed7b68d56d8aa67c71d0289f17c25d14
eksperimental <eksperimental@users.noreply.github.com>
2018-09-09 00:32:50 +0700

    Make Ebert happy

--- test/ex_doc/formatter/html/syntax_rule_test.exs
+++ test/ex_doc/formatter/html/syntax_rule_test.exs
@@ -1,3 +1,4 @@
+@moduledoc false
 defmodule ExDoc.Formatter.HTML.SyntaxRuleTest do
   use ExUnit.Case, async: true
 
@@ -13,42 +14,42 @@ defmodule ExDoc.Formatter.HTML.SyntaxRuleTest do
 
     test "modules" do
       refute run(re(:m), "module.fun/1")
-      assert "Module" == run(re(:m), "Module.fun/1") |> hd()
-      assert "Module.Some" == run(re(:m), "Module.Some.fun/1") |> hd()
+      assert "Module" == run_hd(re(:m), "Module.fun/1")
+      assert "Module.Some" == run_hd(re(:m), "Module.Some.fun/1")
     end
 
     test "functions" do
-      assert "fun" == run(re(:f), "`fun/1`") |> hd()
-      assert "is_FUN_99?" == run(re(:f), "`is_FUN_99?/1`") |> hd()
+      assert "fun" == run_hd(re(:f), "`fun/1`")
+      assert "is_FUN_99?" == run_hd(re(:f), "`is_FUN_99?/1`")
 
-      assert "fun/11" == run(re(:fa), "`fun/11`") |> hd()
-      assert "is_FUN_99?/11" == run(re(:fa), "`is_FUN_99?/11`") |> hd()
+      assert "fun/11" == run_hd(re(:fa), "`fun/11`")
+      assert "is_FUN_99?/11" == run_hd(re(:fa), "`is_FUN_99?/11`")
     end
 
     test "special forms" do
-      assert "/" == run(re(:f), "`/2`") |> hd()
-      assert "." == run(re(:f), "`.2`") |> hd()
-      assert "__ENV__" == run(re(:f), "`__ENV__/0`") |> hd()
+      assert "/" == run_hd(re(:f), "`/2`")
+      assert "." == run_hd(re(:f), "`.2`")
+      assert "__ENV__" == run_hd(re(:f), "`__ENV__/0`")
     end
   end
 
   describe "Erlang: basic parts" do
     test "modules" do
       refute run(re(:m, :erlang), "MODULE.FUN/1")
-      assert ":module" == run(re(:m, :erlang), ":module.fun/1") |> hd()
-      assert ":module_some" == run(re(:m, :erlang), ":module_some.fun/1") |> hd()
+      assert ":module" == run_hd(re(:m, :erlang), ":module.fun/1")
+      assert ":module_some" == run_hd(re(:m, :erlang), ":module_some.fun/1")
     end
 
     test "functions" do
-      assert "fun" == run(re(:f, :erlang), "`fun/1`") |> hd()
-      assert "is_FUN_99?!" == run(re(:f, :erlang), "`is_FUN_99?!/1`") |> hd()
+      assert "fun" == run_hd(re(:f, :erlang), "`fun/1`")
+      assert "is_FUN_99?!" == run_hd(re(:f, :erlang), "`is_FUN_99?!/1`")
 
-      assert "is_fun_99/123" == run(re(:fa, :erlang), "`is_fun_99/123`") |> hd()
+      assert "is_fun_99/123" == run_hd(re(:fa, :erlang), "`is_fun_99/123`")
     end
 
     test "mfa" do
       assert ":my_erlang_module.is_fun!/123" ==
-               run(re(:mfa, :erlang), "`:my_erlang_module.is_fun!/123`") |> hd()
+               run_hd(re(:mfa, :erlang), "`:my_erlang_module.is_fun!/123`")
     end
   end
 
@@ -63,22 +64,26 @@ defmodule ExDoc.Formatter.HTML.SyntaxRuleTest do
       mfa = re_source(:mfa)
 
       assert "[`example`](`Mod.example/1`)" ==
-               run(re({:custom_link, mfa}, :markdown), "[`example`](`Mod.example/1`)") |> hd()
+               run_hd(re({:custom_link, mfa}, :markdown), "[`example`](`Mod.example/1`)")
     end
 
     test "normal links - Erlang" do
       mfa = re_source(:mfa, :erlang)
       refute run(re({:normal_link, mfa}, :markdown), "[`example`](`:mod.example/1`)")
-      assert run(re({:normal_link, mfa}, :markdown), "`:mod.example/1`")
+      assert run_hd(re({:normal_link, mfa}, :markdown), "`:mod.example/1`")
     end
 
     test "custom links - Erlang" do
       mfa = re_source(:mfa, :erlang)
 
       assert "[`example`](`:mod.example/1`)" ==
-               run(re({:custom_link, mfa}, :markdown), "[`example`](`:mod.example/1`)") |> hd()
+               run_hd(re({:custom_link, mfa}, :markdown), "[`example`](`:mod.example/1`)")
 
       refute run(re({:custom_link, mfa}, :markdown), "`:mod.example/1`")
     end
+  end
+
+  defp run_hd(regex, string) do
+    hd(Regex.run(regex, string))
   end
 end
