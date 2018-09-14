938ec2a38eac14cb3a559260d6130250de7a59c4
José Valim <jose.valim@gmail.com>
2018-09-14 23:16:34 +0200

    Update html_test.exs

--- test/ex_doc/formatter/html_test.exs
+++ test/ex_doc/formatter/html_test.exs
@@ -342,23 +342,12 @@ defmodule ExDoc.Formatter.HTMLTest do
       assert content =~ ~r{"id":"extrapage","title":"Extra Page Title"}
     end
 
-    test "with api-reference" do
-      generate_docs(doc_config())
-      content = File.read!("#{output_dir()}/api-reference.html")
-      assert content =~ ~r{<title>API Reference – Elixir v1.0.1</title>}
-      content = read_wildcard!("#{output_dir()}/dist/sidebar_items-*.js")
-      assert content =~ ~r{"id":"api-reference","title":"API Reference"}
-    end
-
     test "without api-reference" do
       generate_docs(
         doc_config(api_reference: false, extras: ["test/fixtures/README.md"], main: "readme")
       )
 
-      assert_raise File.Error, fn ->
-        File.read!("#{output_dir()}/api-reference.html")
-      end
-
+      refute File.exists?("#{output_dir()}/api-reference.html")
       content = read_wildcard!("#{output_dir()}/dist/sidebar_items-*.js")
       refute content =~ ~r{"id":"api-reference","title":"API Reference"}
     end
