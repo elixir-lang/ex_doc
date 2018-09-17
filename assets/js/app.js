9f03ed4a2d4019f0b19a24a64bf64f244dff269c
Gabe Jackson <gj@mail.co.de>
2018-09-17 10:56:20 -0400

    Register Handlebars helpers

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@
 // ------------
 
 import $ from 'jquery'
+import Handlebars from 'handlebars/runtime'
+
 import hljs from 'highlight.js/lib/highlight'
 import bash from 'highlight.js/lib/languages/bash'
 import css from 'highlight.js/lib/languages/css'
@@ -15,6 +17,12 @@ import markdown from 'highlight.js/lib/languages/markdown'
 import sql from 'highlight.js/lib/languages/sql'
 import xml from 'highlight.js/lib/languages/xml'
 
+import isArray from './template-helpers/isArray'
+import isEmptyNode from './template-helpers/isEmptyNode'
+import isLocal from './template-helpers/isLocal'
+import newGroup from './template-helpers/newGroup'
+import showSummary from './template-helpers/showSummary'
+
 import {initialize as initEvents} from './events'
 import {initialize as initSidebar} from './sidebar'
 import {initialize as initNightMode} from './night'
@@ -23,7 +31,14 @@ import {initialize as initMakeup} from './makeup'
 window.$ = $
 
 $(() => {
-  // Setup Highlight.js
+  // Set up Handlebars.js
+  Handlebars.registerHelper('isArray', isArray)
+  Handlebars.registerHelper('isEmptyNode', isEmptyNode)
+  Handlebars.registerHelper('isLocal', isLocal)
+  Handlebars.registerHelper('newGroup', newGroup)
+  Handlebars.registerHelper('showSummary', showSummary)
+
+  // Set up Highlight.js
   hljs.configure({
     tabReplace: '    ', // 4 spaces
     languages: []       // disable auto-detect
