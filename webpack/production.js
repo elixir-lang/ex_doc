be8c81e0b49b6e164fd6e5087a202ca0257b8450
Gabe Jackson <gj@mail.co.de>
2018-09-17 00:55:20 -0400

    Add in UglifyJS for parity with Gulpfile

--- webpack/production.js
+++ webpack/production.js
@@ -1,6 +1,15 @@
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const merge = require('webpack-merge')
 const common = require('./common.js')
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true
+      })
+    ]
+  }
 })
