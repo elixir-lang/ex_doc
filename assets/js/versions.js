3049b1cfec4fcb672012086a8d6c9e234acbc55a
Michał Łępicki <michal.lepicki@gmail.com>
2019-02-07 19:43:01 +0100

    fix eslint errors

--- assets/js/versions.js
+++ assets/js/versions.js
@@ -4,41 +4,43 @@
 // ------------
 import $ from 'jquery'
 
-
-function version_option (current_version) {
-  return function (version_object) {
-    return '<option ' + option_attributes(version_object, version_object.version == current_version) + '>' +
-      version_object.version +
+function versionOption (currentVersion) {
+  return function (versionObject) {
+    return '<option ' + optionAttributes(versionObject, versionObject.version === currentVersion) + '>' +
+      versionObject.version +
       '</option>'
   }
 }
 
-function option_attributes (version_object, is_current_version) {
-  return `value="${version_object.url}"${(is_current_version ? ' selected disabled' : '')}`
+function optionAttributes (versionObject, isCurrentVersion) {
+  return `value="${versionObject.url}"${(isCurrentVersion ? ' selected disabled' : '')}`
 }
 
 // Public Methods
 // --------------
-export function initialize() {
-  if (typeof versionNodes !== 'undefined') {
-    let sidebar_projectVersion = $('.sidebar-projectVersion')
-    let current_version = sidebar_projectVersion.text().trim()
 
-    if (!versionNodes.find(function (element) { return element.version == current_version })) {
-      versionNodes.unshift({ version: current_version, url: '#' })
+/* eslint-disable no-undef */
+export function initialize () {
+  if (typeof versionNodes !== 'undefined') {
+    let sidebarProjectVersion = $('.sidebar-projectVersion')
+    let currentVersion = sidebarProjectVersion.text().trim()
+
+    if (!versionNodes.find(function (element) { return element.version === currentVersion })) {
+      versionNodes.unshift({ version: currentVersion, url: '#' })
     }
 
-    let versions_dropdown =
+    let versionsDropdown =
       '<form autocomplete="off">' +
       '<select class="sidebar-projectVersionsDropdown">' +
-      (versionNodes.map(version_option(current_version)).join('')) +
+      (versionNodes.map(versionOption(currentVersion)).join('')) +
       '</select>' +
       '</form>'
-    sidebar_projectVersion.text('')
-    sidebar_projectVersion.append(versions_dropdown)
+    sidebarProjectVersion.text('')
+    sidebarProjectVersion.append(versionsDropdown)
 
     $('.sidebar-projectVersionsDropdown').change(function () {
-      window.location.href = $(this).val();
+      window.location.href = $(this).val()
     })
   }
 }
+/* eslint-enable no-undef */
