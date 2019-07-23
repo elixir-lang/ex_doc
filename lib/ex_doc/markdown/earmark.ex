6ab4a641ffddc352533c1fba81b100603d311a06
José Valim <jose.valim@gmail.com>
2019-07-23 09:46:05 +0200

    Update earmark.ex

--- lib/ex_doc/markdown/earmark.ex
+++ lib/ex_doc/markdown/earmark.ex
@@ -21,7 +21,9 @@ defmodule ExDoc.Markdown.Earmark do
   end
 
   @doc """
-  Earmark specific options:
+  Generate HTML output.
+
+  ## Options
 
     * `:gfm` - boolean. Turns on Github Flavored Markdown extensions. True by default
 
@@ -29,11 +31,13 @@ defmodule ExDoc.Markdown.Earmark do
       breaks significant (so every line in the input is a new line in the output)
 
     * `:smartypants` - boolean. Turns on smartypants processing, so quotes become curly,
-      two or three hyphens become en and em dashes, and so on. True by default
+      two or three hyphens become en and em dashes, and so on. False by default
 
     * `:plugins` - map of strings to modules. Register custom plugins to be used with
       Earmark. See [Plugins](http://github.com/pragdave/earmark#plugins) for details on
       how to write custom plugins.
+
+    * `:highlight_tag` - the tag used for highlighting code, defaults to "span"
 
   """
   def to_html(text, opts) do
@@ -43,19 +47,13 @@ defmodule ExDoc.Markdown.Earmark do
         line: Keyword.get(opts, :line, 1),
         file: Keyword.get(opts, :file),
         breaks: Keyword.get(opts, :breaks, false),
-        smartypants: Keyword.get(opts, :smartypants, true),
-        # As discussed, this enables pure links
+        smartypants: Keyword.get(opts, :smartypants, false),
         pure_links: true,
-        # Alternatively this would not enable them and surpress deprecation warnings
-        # pure_links: false,
-        # Or the default behavior, deprecation warnings
-        # pure_links: nil,
-
         plugins: Keyword.get(opts, :plugins, %{})
       )
 
     text
     |> Earmark.as_html!(options)
-    |> ExDoc.Highlighter.highlight_code_blocks()
+    |> ExDoc.Highlighter.highlight_code_blocks(opts)
   end
 end
