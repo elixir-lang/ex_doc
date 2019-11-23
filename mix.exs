a2fc5f231730a205ec64aba39279d538d22d0711
Robert Dober <robert.dober@gmail.com>
2019-11-23 15:03:14 +0100

    Earmark version bump (#1109)

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule ExDoc.Mixfile do
 
   defp deps do
     [
-      {:earmark, "~> 1.3.3 or ~> 1.4"},
+      {:earmark, "~> 1.4"},
       {:makeup_elixir, "~> 0.14"},
       {:cmark, "~> 0.5", only: :test},
       {:excoveralls, "~> 0.3", only: :test}
