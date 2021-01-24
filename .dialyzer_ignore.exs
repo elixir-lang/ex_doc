410ba1a7b31deaa8dce5e29fc4b324f55443a198
José Valim <jose.valim@gmail.com>
2021-01-24 18:09:11 +0100

    Delete .dialyzer_ignore.exs

--- .dialyzer_ignore.exs
+++
@@ -1,4 +0,0 @@
-[
-  # Error message for future unsupported versions of Elixir
-  ~r{lib/ex_doc/retriever.ex:\d+:pattern_match_cov The pattern variable _ can never match the type, because it is covered by previous clauses.}
-]
