ee13693cbe8d0759ba15289132343c9dead8b1b6
José Valim <jose.valim@dashbit.co>
2021-10-26 13:32:06 +0200

    Run linter

--- assets/js/copy-button.js
+++ assets/js/copy-button.js
@@ -1,6 +1,6 @@
 import { qsAll } from './helpers'
 
-const BUTTON = `<button class="copy-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg></button>`;
+const BUTTON = `<button class="copy-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg></button>`
 
 /**
  * Initializes copy buttons.
@@ -20,8 +20,8 @@ function addCopyButtons () {
   Array.from(qsAll('.copy-button')).forEach(button =>
     button.addEventListener('click', () => {
       navigator.clipboard.writeText(button.parentElement.querySelector('code').textContent)
-      button.classList.add("clicked")
-      setTimeout(() => button.classList.remove("clicked"), 3000)
+      button.classList.add('clicked')
+      setTimeout(() => button.classList.remove('clicked'), 3000)
     })
   )
 }
