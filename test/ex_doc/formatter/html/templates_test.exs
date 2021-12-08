d77ee66cc656df722dc1f5a63ad98536239cd38c
Eksperimental <eksperimental@autistici.org>
2021-12-07 21:41:37 -0500

    Use Struct instead of __struct__/0 in sidebar
    Closes #1433

--- test/ex_doc/formatter/html/templates_test.exs
+++ test/ex_doc/formatter/html/templates_test.exs
@@ -218,6 +218,7 @@ defmodule ExDoc.Formatter.HTML.TemplatesTest do
       assert content =~ ~r("id":"CompiledWithDocs".*"key":"functions".*"example/2")ms
       assert content =~ ~r("id":"CompiledWithDocs".*"key":"functions".*"example_without_docs/0")ms
       assert content =~ ~r("id":"CompiledWithDocs.Nested")ms
+      assert content =~ ~r(\{"anchor":"__struct__/0","id":"%CompiledWithDocs\{\}"\})ms
     end
 
     test "outputs nodes grouped based on metadata" do
