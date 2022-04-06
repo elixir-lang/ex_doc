b897af150f856db151178b50ccd770aa2770eb64
David Bernheisel <david@bernheisel.com>
2022-04-05 22:37:14 -0400

    Add toast when changing theme via keyboard

---
+++ assets/js/toast.js
@@ -0,0 +1,20 @@
+var toastTimer = null;
+const toast = document.getElementById("toast");
+toast.addEventListener('click', (event) => {
+  clearTimeout(toastTimer);
+  event.target.classList.remove("show");
+})
+
+export function showToast(message) {
+  if (toast) {
+    clearTimeout(toastTimer);
+    toast.innerText = message;
+    toast.classList.add("show");
+
+    toastTimer = setTimeout(() => {
+      toast.classList.remove("show");
+      // wait for transition animation
+      toastTimer = setTimeout(() => toast.innerText = '', 1000)
+    }, 5000);
+  }
+}
