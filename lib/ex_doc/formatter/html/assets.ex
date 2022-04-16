a2fba022d4a1a5fa295e324d7b65711e0e1c752a
Timothy Vanderaerden <timothyvanderaerden@gmail.com>
2022-04-16 19:34:37 +0200

    Use strict pattern for license inclusion in doc build

--- lib/ex_doc/formatter/html/assets.ex
+++ lib/ex_doc/formatter/html/assets.ex
@@ -8,12 +8,12 @@ defmodule ExDoc.Formatter.HTML.Assets do
     |> Enum.map(&{Path.basename(&1), File.read!(&1)})
   end
 
-  def dist(proglang), do: dist_js() ++ dist_css(proglang) ++ dist_txt()
+  def dist(proglang), do: dist_js() ++ dist_css(proglang) ++ dist_license()
 
   defp dist_js(), do: embed_pattern("dist/*.js")
   defp dist_css(:elixir), do: embed_pattern("dist/elixir-*.css")
   defp dist_css(:erlang), do: embed_pattern("dist/erlang-*.css")
-  defp dist_txt(), do: embed_pattern("dist/*.txt")
+  defp dist_license(), do: embed_pattern("dist/*.LICENSE.txt")
 
   def fonts, do: embed_pattern("fonts/*")
 end
