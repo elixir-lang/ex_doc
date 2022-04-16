206f878727340ed812808257f24a6c511585644c
Timothy Vanderaerden <timothyvanderaerden@gmail.com>
2022-04-16 17:29:51 +0200

    Include Javascript Script libraries in doc build

--- lib/ex_doc/formatter/html/assets.ex
+++ lib/ex_doc/formatter/html/assets.ex
@@ -8,11 +8,12 @@ defmodule ExDoc.Formatter.HTML.Assets do
     |> Enum.map(&{Path.basename(&1), File.read!(&1)})
   end
 
-  def dist(proglang), do: dist_js() ++ dist_css(proglang)
+  def dist(proglang), do: dist_js() ++ dist_css(proglang) ++ dist_txt()
 
   defp dist_js(), do: embed_pattern("dist/*.js")
   defp dist_css(:elixir), do: embed_pattern("dist/elixir-*.css")
   defp dist_css(:erlang), do: embed_pattern("dist/erlang-*.css")
+  defp dist_txt(), do: embed_pattern("dist/*.txt")
 
   def fonts, do: embed_pattern("fonts/*")
 end
