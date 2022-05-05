f6e2b3090cac6d26254b8587bb11ea4febbcad8b
Eksperimental <eksperimental@autistici.org>
2022-05-05 08:36:34 +0000

    Avoid race conditions in tests (#1569)
    Use tmp_dir in tests to avoid CI failures:
    https://github.com/elixir-lang/ex_doc/runs/6293776556?check_suite_focus=true

--- test/mix/tasks/docs_test.exs
+++ test/mix/tasks/docs_test.exs
@@ -3,109 +3,123 @@ Mix.shell(Mix.Shell.Process)
 
 defmodule Mix.Tasks.DocsTest do
   # Cannot run concurrently due to Mix compile/deps calls
-  use ExUnit.Case
+  use ExUnit.Case, async: false
 
-  def run(args, opts) do
+  @moduletag :tmp_dir
+
+  def run(context, args, opts) do
+    opts = Keyword.put_new(opts, :output, context[:tmp_dir])
     Mix.Tasks.Docs.run(args, opts, &{&1, &2, &3})
   end
 
-  test "inflects values from app and version" do
+  test "inflects values from app and version", context do
     assert [
              {"ex_doc", "0.1.0", [formatter: "html", deps: _, apps: _, source_beam: _]},
              {"ex_doc", "0.1.0", [formatter: "epub", deps: _, apps: _, source_beam: _]}
-           ] = run([], app: :ex_doc, version: "0.1.0")
+           ] = run(context, [], app: :ex_doc, version: "0.1.0")
   end
 
-  test "accepts multiple formatters from CLI" do
+  test "accepts multiple formatters from CLI", context do
     assert [
              {"ex_doc", "0.1.0", [formatter: "html", deps: _, apps: _, source_beam: _]},
              {"ex_doc", "0.1.0", [formatter: "epub", deps: _, apps: _, source_beam: _]}
-           ] = run(["-f", "html", "-f", "epub"], app: :ex_doc, version: "0.1.0")
+           ] = run(context, ["-f", "html", "-f", "epub"], app: :ex_doc, version: "0.1.0")
   end
 
-  test "accepts multiple formatters from config" do
+  test "accepts multiple formatters from config", context do
     assert [
              {"ex_doc", "0.1.0",
               [formatter: "html", deps: _, apps: _, source_beam: _, formatters: _]},
              {"ex_doc", "0.1.0",
               [formatter: "epub", deps: _, apps: _, source_beam: _, formatters: _]}
-           ] = run([], app: :ex_doc, version: "0.1.0", docs: [formatters: ["html", "epub"]])
+           ] =
+             run(context, [], app: :ex_doc, version: "0.1.0", docs: [formatters: ["html", "epub"]])
   end
 
-  test "uses the given name" do
+  test "uses the given name", context do
     assert [
              {"ExDoc", "0.1.0", [formatter: "html", deps: _, apps: _, source_beam: _]},
              {"ExDoc", "0.1.0", [formatter: "epub", deps: _, apps: _, source_beam: _]}
-           ] = run([], app: :ex_doc, version: "0.1.0", name: "ExDoc")
+           ] = run(context, [], app: :ex_doc, version: "0.1.0", name: "ExDoc")
   end
 
-  test "accepts modules in :main" do
+  test "accepts modules in :main", context do
     assert [
              {"ex_doc", "dev",
               [formatter: "html", deps: _, main: "Sample", apps: _, source_beam: _]},
              {"ex_doc", "dev",
               [formatter: "epub", deps: _, main: "Sample", apps: _, source_beam: _]}
-           ] = run([], app: :ex_doc, docs: [main: Sample])
+           ] = run(context, [], app: :ex_doc, docs: [main: Sample])
   end
 
-  test "accepts files in :main" do
+  test "accepts files in :main", context do
     assert [
              {"ex_doc", "dev",
               [formatter: "html", deps: _, apps: _, source_beam: _, main: "another"]},
              {"ex_doc", "dev",
               [formatter: "epub", deps: _, apps: _, source_beam: _, main: "another"]}
-           ] = run([], app: :ex_doc, docs: [main: "another"])
+           ] = run(context, [], app: :ex_doc, docs: [main: "another"])
   end
 
-  test "accepts output in :output" do
+  test "accepts output in :output", %{tmp_dir: tmp_dir} = context do
+    [{_, _, html_options}, {_, _, epub_options}] =
+      run_results = run(context, [], app: :ex_doc, docs: [output: tmp_dir <> "/hello"])
+
     assert [
-             {"ex_doc", "dev",
-              [formatter: "html", deps: _, apps: _, source_beam: _, output: "hello"]},
-             {"ex_doc", "dev",
-              [formatter: "epub", deps: _, apps: _, source_beam: _, output: "hello"]}
-           ] = run([], app: :ex_doc, docs: [output: "hello"])
+             {"ex_doc", "dev", [formatter: "html", deps: _, apps: _, source_beam: _, output: _]},
+             {"ex_doc", "dev", [formatter: "epub", deps: _, apps: _, source_beam: _, output: _]}
+           ] = run_results
+
+    assert html_options[:output] == "#{tmp_dir}/hello"
+    assert epub_options[:output] == "#{tmp_dir}/hello"
   end
 
-  test "parses output with lower preference than options" do
+  test "parses output with lower preference than options", %{tmp_dir: tmp_dir} = context do
+    output = tmp_dir <> "/world"
+
+    [{_, _, html_options}, {_, _, epub_options}] =
+      run_results = run(context, ["-o", "#{output}"], app: :ex_doc, docs: [output: output])
+
     assert [
-             {"ex_doc", "dev",
-              [formatter: "html", deps: _, apps: _, source_beam: _, output: "world"]},
-             {"ex_doc", "dev",
-              [formatter: "epub", deps: _, apps: _, source_beam: _, output: "world"]}
-           ] = run(~w(-o world), app: :ex_doc, docs: [output: "world"])
+             {"ex_doc", "dev", [formatter: "html", deps: _, apps: _, source_beam: _, output: _]},
+             {"ex_doc", "dev", [formatter: "epub", deps: _, apps: _, source_beam: _, output: _]}
+           ] = run_results
+
+    assert html_options[:output] == "#{tmp_dir}/world"
+    assert epub_options[:output] == "#{tmp_dir}/world"
   end
 
-  test "includes dependencies" do
+  test "includes dependencies", context do
     assert [
              {"ex_doc", "dev", [formatter: "html", deps: deps, apps: _, source_beam: _]},
              {"ex_doc", "dev", [formatter: "epub", deps: deps, apps: _, source_beam: _]}
-           ] = run([], app: :ex_doc, docs: [])
+           ] = run(context, [], app: :ex_doc, docs: [])
 
     assert List.keyfind(deps, :earmark_parser, 0) ==
              {:earmark_parser,
               "https://hexdocs.pm/earmark_parser/#{Application.spec(:earmark_parser, :vsn)}/"}
   end
 
-  test "allows custom dependency paths" do
+  test "allows custom dependency paths", context do
     assert [
              {"ex_doc", "dev", [formatter: "html", deps: deps, apps: _, source_beam: _]},
              {"ex_doc", "dev", [formatter: "epub", deps: deps, apps: _, source_beam: _]}
-           ] = run([], app: :ex_doc, docs: [deps: [earmark_parser: "foo"]])
+           ] = run(context, [], app: :ex_doc, docs: [deps: [earmark_parser: "foo"]])
 
     assert List.keyfind(deps, :earmark_parser, 0) ==
              {:earmark_parser, "foo"}
   end
 
-  test "accepts lazy docs" do
+  test "accepts lazy docs", context do
     assert [
              {"ex_doc", "dev",
               [formatter: "html", deps: _, apps: _, source_beam: _, main: "another"]},
              {"ex_doc", "dev",
               [formatter: "epub", deps: _, apps: _, source_beam: _, main: "another"]}
-           ] = run([], app: :ex_doc, docs: fn -> [main: "another"] end)
+           ] = run(context, [], app: :ex_doc, docs: fn -> [main: "another"] end)
   end
 
-  test "accepts options from root" do
+  test "accepts options from root", context do
     # accepted options are: `app`, `name`, `source_url`, `homepage_url`, `version`
     assert [
              {"ExDoc", "1.2.3-dev",
@@ -127,7 +141,7 @@ defmodule Mix.Tasks.DocsTest do
                 source_url: "https://github.com/elixir-lang/ex_doc"
               ]}
            ] =
-             run([],
+             run(context, [],
                app: :ex_doc,
                name: "ExDoc",
                source_url: "https://github.com/elixir-lang/ex_doc",
@@ -135,10 +149,10 @@ defmodule Mix.Tasks.DocsTest do
                version: "1.2.3-dev"
              )
 
-    assert [{"ex_doc", "dev", _}, {"ex_doc", "dev", _}] = run([], app: :ex_doc)
+    assert [{"ex_doc", "dev", _}, {"ex_doc", "dev", _}] = run(context, [], app: :ex_doc)
   end
 
-  test "supports umbrella project" do
+  test "supports umbrella project", context do
     Mix.Project.in_project(:umbrella, "test/fixtures/umbrella", fn _mod ->
       [
         {"umbrella", "dev",
@@ -155,11 +169,11 @@ defmodule Mix.Tasks.DocsTest do
            apps: [:bar, :foo],
            source_beam: _
          ]}
-      ] = run([], app: :umbrella, apps_path: "apps/", docs: [])
+      ] = run(context, [], app: :umbrella, apps_path: "apps/", docs: [])
     end)
   end
 
-  test "supports umbrella project with ignore_apps" do
+  test "supports umbrella project with ignore_apps", context do
     Mix.Project.in_project(:umbrella, "test/fixtures/umbrella", fn _mod ->
       [
         {"umbrella", "dev",
@@ -178,7 +192,7 @@ defmodule Mix.Tasks.DocsTest do
            source_beam: _,
            ignore_apps: [:foo]
          ]}
-      ] = run([], app: :umbrella, apps_path: "apps/", docs: [ignore_apps: [:foo]])
+      ] = run(context, [], app: :umbrella, apps_path: "apps/", docs: [ignore_apps: [:foo]])
     end)
   end
 end
