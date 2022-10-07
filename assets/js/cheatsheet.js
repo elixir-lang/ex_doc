89ee6814530079ce09535a15b5d1cd25fb3825d1
paulo-valim <plvalim@gmail.com>
2022-10-07 14:57:06 +0200

    finish columns layout

--- assets/js/cheatsheet.js
+++ assets/js/cheatsheet.js
@@ -3,6 +3,6 @@ console.log(urlCheat)
 const cheatsheet = document.getElementById('content');
 console.log(cheatsheet);
 if (urlCheat.indexOf('cheatsheet.html') >= 0) {
-  cheatsheet.classList.remove('content-inner')
+//  cheatsheet.classList.remove('content-inner')
   cheatsheet.classList.add('content-cheatsheet')
 }
