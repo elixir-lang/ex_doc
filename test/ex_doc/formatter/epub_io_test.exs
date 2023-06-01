229c1636a94290805a8280af8944d84e39b7bbfc
Vinícius Müller <vinigm.nho@gmail.com>
2023-06-01 10:11:46 -0300

    Simplify IO test modules

--- test/ex_doc/formatter/epub_io_test.exs
+++ test/ex_doc/formatter/epub_io_test.exs
@@ -3,53 +3,36 @@ defmodule ExDoc.Formatter.EPUBIOTest do
 
   @moduletag :tmp_dir
 
-  test "succeeds if trying to write into an empty existing directory", context do
-    config = doc_config(context)
-
-    new_output = config[:output] <> "/new-dir"
-    File.mkdir_p!(new_output)
-
-    new_config = Keyword.put(config, :output, new_output)
-
-    refute ExUnit.CaptureIO.capture_io(:stderr, fn ->
-             generate_docs(new_config)
-           end) =~ "ExDoc is outputting to an existing directory"
-  end
-
-  test "warns if trying to write into existing directory with files", context do
-    config = doc_config(context)
-    new_output = config[:output] <> "/new-dir"
-
-    File.mkdir_p!(new_output)
-    File.touch!(Path.join(new_output, "dummy-file"))
-
-    new_config = Keyword.put(config, :output, new_output)
+  test "succeeds if trying to write into an empty existing directory", %{tmp_dir: tmp_dir} do
+    File.mkdir!("#{tmp_dir}/doc")
 
     assert ExUnit.CaptureIO.capture_io(:stderr, fn ->
-             generate_docs(new_config)
+             generate_docs(tmp_dir)
+           end) =~ ""
+  end
+
+  test "warns if trying to write into existing directory with files", %{tmp_dir: tmp_dir} do
+    File.mkdir!("#{tmp_dir}/doc")
+    File.touch!("#{tmp_dir}/doc/foo.txt")
+
+    assert ExUnit.CaptureIO.capture_io(:stderr, fn ->
+             generate_docs(tmp_dir)
            end) =~ "ExDoc is outputting to an existing directory"
 
     # Warn only once
-    refute ExUnit.CaptureIO.capture_io(:stderr, fn ->
-             generate_docs(new_config)
-           end) =~ "ExDoc is outputting to an existing directory"
+    assert ExUnit.CaptureIO.capture_io(:stderr, fn ->
+             generate_docs(tmp_dir)
+           end) =~ ""
   end
 
-  defp generate_docs(config) do
-    config = Keyword.put_new(config, :skip_undefined_reference_warnings_on, ["Warnings"])
-    ExDoc.generate_docs(config[:project], config[:version], config)
-  end
-
-  defp doc_config(%{tmp_dir: tmp_dir} = _context) do
-    [
-      app: :elixir,
-      project: "Elixir",
-      version: "1.0.1",
+  defp generate_docs(tmp_dir) do
+    config = [
+      app: :foo,
       formatter: "epub",
-      output: tmp_dir <> "/epub",
-      source_beam: "test/tmp/beam",
-      extras: ["test/fixtures/README.md"],
-      skip_undefined_reference_warnings_on: ["Warnings"]
+      output: "#{tmp_dir}/doc",
+      source_beam: "#{tmp_dir}/ebin",
     ]
+
+    ExDoc.generate_docs("Foo", "1.0.0", config)
   end
 end
