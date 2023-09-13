7be1ee0701c0af6fb2e1bf0dd9f4349fc9d174e6
Angelika Tyborska <angelikatyborska@fastmail.com>
2023-09-13 10:16:41 +0200

    Add screen reader feedback to copy button

--- assets/js/copy-button.js
+++ assets/js/copy-button.js
@@ -1,6 +1,6 @@
 import { qsAll } from './helpers'
 
-const BUTTON = '<button class="copy-button" aria-label="copy"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg></button>'
+const BUTTON = '<button class="copy-button"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg><span class="sr-only">copy</span><span aria-live="polite"></span></button>'
 
 /**
  * Initializes copy buttons.
@@ -22,6 +22,7 @@ function addCopyButtons () {
   Array.from(qsAll('.copy-button')).forEach(button => {
     let timeout
     button.addEventListener('click', () => {
+      const ariaLiveContent = button.querySelector('[aria-live]')
       timeout && clearTimeout(timeout)
 
       const text =
@@ -32,7 +33,11 @@ function addCopyButtons () {
 
       navigator.clipboard.writeText(text)
       button.classList.add('clicked')
-      timeout = setTimeout(() => button.classList.remove('clicked'), 3000)
+      ariaLiveContent.innerHTML = 'Copied! &#x2713;'
+      timeout = setTimeout(() => {
+        button.classList.remove('clicked')
+        ariaLiveContent.innerHTML = ''
+      }, 3000)
     })
   })
 }
