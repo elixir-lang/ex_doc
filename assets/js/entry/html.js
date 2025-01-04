96d1fea4c99438c982014fba5f44d57811681b98
Liam Mitchell <git@liam.geek.nz>
2025-01-04 14:46:02 +0100

    Init toast lazily

--- assets/js/entry/html.js
+++ assets/js/entry/html.js
@@ -10,7 +10,6 @@ import { initialize as initTheme } from '../theme'
 import { initialize as initMakeup } from '../makeup'
 import { initialize as initKeyboardShortcuts } from '../keyboard-shortcuts'
 import { initialize as initQuickSwitch } from '../quick-switch'
-import { initialize as initToast } from '../toast'
 import { initialize as initTooltips } from '../tooltips/tooltips'
 import { initialize as initHintsPage } from '../tooltips/hint-page'
 import { initialize as initCopyButton } from '../copy-button'
@@ -72,7 +71,6 @@ onDocumentReady(() => {
     initVersions()
     initKeyboardShortcuts()
     initQuickSwitch()
-    initToast()
 
     initSidebarDrawer()
     initSidebarContent()
