4816fb7927ce4bbdd99c302c66f0d7ad4b1077f2
José Valim <jose.valim@dashbit.co>
2025-01-27 16:01:33 +0100

    Also generate Erlang files in prerelease

--- test/prerelease.sh
+++ test/prerelease.sh
@@ -10,8 +10,11 @@ TAR=$(ls ex_doc-*.tar | head -n 1)
 rm -rf $PKG && mkdir -p $PKG/contents && tar xf $TAR -C $PKG && tar xzf $PKG/contents.tar.gz -C $PKG/contents
 
 # compile and build docs
-cd $PKG/contents && MIX_ENV=prod mix do deps.get --only prod + compile + docs
+cd $PKG/contents
+MIX_ENV=prod mix do deps.get --only prod + compile + docs
+MIX_ENV=prod mix docs --proglang erlang --output doc/erlang
 
 # run assertions
 test -f doc/index.html || echo "doc/index.html missing"
+test -f doc/erlang/index.html || echo "doc/erlang/index.html missing"
 test -f doc/ExDoc.epub || echo "doc/ExDoc.epub missing"
