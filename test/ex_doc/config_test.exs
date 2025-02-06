eee74077042f446f99a11c396d4485017108ffea
Roberto Aloi <robertoaloi@meta.com>
2025-02-06 09:15:14 +0100

    Add config test for groups_for_modules

--- test/ex_doc/config_test.exs
+++ test/ex_doc/config_test.exs
@@ -113,5 +113,43 @@ defmodule ExDoc.ConfigTest do
       config = build(source_url_pattern: "a%{line}b%{path}c")
       assert config.source_url_pattern.("foo.ex", 123) == "a123bfoo.exc"
     end
+
+    test "groups_for_modules" do
+      # Using real applications, since we load them to extract the corresponding list of modules
+      stdlib = :stdlib
+      kernel = :kernel
+      custom_group = :custom_group
+
+      groups_for_modules = fn config, key ->
+        List.keyfind(config.groups_for_modules, to_string(key), 0)
+      end
+
+      # Single app, no custom grouping
+      config = build(apps: [stdlib])
+      assert groups_for_modules.(config, stdlib) == nil
+      assert groups_for_modules.(config, custom_group) == nil
+
+      # Single app, custom grouping
+      config = build(apps: [stdlib], groups_for_modules: [{"custom_group", ["module_1"]}])
+      assert groups_for_modules.(config, stdlib) == nil
+      assert groups_for_modules.(config, custom_group) == {"custom_group", ["module_1"]}
+
+      # Multiple apps, no custom grouping
+      config = build(apps: [stdlib, kernel])
+      stdlib_groups = groups_for_modules.(config, stdlib)
+      kernel_groups = groups_for_modules.(config, kernel)
+      assert match?({"stdlib", _}, stdlib_groups)
+      assert match?({"kernel", _}, kernel_groups)
+      {"stdlib", stdlib_modules} = stdlib_groups
+      {"kernel", kernel_modules} = kernel_groups
+      assert Enum.member?(stdlib_modules, :gen_server)
+      assert Enum.member?(kernel_modules, :file)
+
+      # Multiple apps, custom grouping
+      config = build(apps: [stdlib, kernel], groups_for_modules: [{"custom_group", ["module_1"]}])
+      assert groups_for_modules.(config, stdlib) == nil
+      assert groups_for_modules.(config, kernel) == nil
+      assert groups_for_modules.(config, custom_group) == {"custom_group", ["module_1"]}
+    end
   end
 end
