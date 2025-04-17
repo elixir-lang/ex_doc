25521da4a462d6027175090557990caf54c176ab
lud <ludovic@demblans.com>
2025-04-17 06:47:25 +0200

    enforce group titles as binaries

--- lib/ex_doc/retriever.ex
+++ lib/ex_doc/retriever.ex
@@ -378,14 +378,14 @@ group = normalize_group(group_for_doc.(metadata) || doc_data.default_group)
     case group do
       %{title: title, description: description}
       when is_binary(title) and (is_binary(description) or is_nil(description)) ->
-        group
+        %{group | title: title, description: description}
 
       kw when is_list(kw) ->
         true = Keyword.keyword?(kw)
-        %{title: Keyword.fetch!(kw, :title), description: kw[:description]}
+        %{title: to_string(Keyword.fetch!(kw, :title)), description: kw[:description]}
 
       title when is_binary(title) when is_atom(title) ->
-        %{title: title, description: nil}
+        %{title: to_string(title), description: nil}
     end
   end
 end
